ff4a0cba5ad9b6db3ce12a17891cd82ea3675c0b
kmgyu <lidersy961@gmail.com>
2025-10-12 00:00:29 +0900

    fix: 이슈 수정 - email을 username으로 수정 - backdoor 추가 - 로깅 추가

--- src/main/java/batch/batchapplication/auth/service/AuthUserDetailService.java
+++ src/main/java/batch/batchapplication/auth/service/AuthUserDetailService.java
@@ -3,6 +3,7 @@ package batch.batchapplication.auth.service;
 import batch.batchapplication.auth.domain.User;
 import batch.batchapplication.auth.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,22 +12,23 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AuthUserDetailService implements UserDetailsService {
   private final UserRepository userRepository;
 
   @Override
   @Transactional(readOnly = true)
-  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-//    log.debug("Spring Security loadUserByUsername 호출: email={}", email);
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    log.info("Spring Security loadUserByUsername 호출: username={}", username);
 
-    User user = userRepository.findByEmail(email)
+    User user = userRepository.findByUsername(username)
             .orElseThrow(() -> {
-//              log.warn("Spring Security - 사용자 없음: email={}", email);
-              return new UsernameNotFoundException("User not found: " + email);
+              log.warn("Spring Security - 사용자 없음: username={}", username);
+              return new UsernameNotFoundException("User not found: " + username);
             });
 
-//    log.debug("Spring Security - 사용자 로드 성공: email={}, authorities={}",
-//            email, user.getAuthorities());
+    log.info("Spring Security - 사용자 로드 성공: username={}, authorities={}",
+            username, user.getAuthorities());
     return user;
   }
 }
