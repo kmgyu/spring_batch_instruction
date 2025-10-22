fd1bad42c9d284fc0dcd12f2075f102caadde9f0
kmgyu <lidersy961@gmail.com>
2025-10-22 17:49:20 +0900

    fix: actuator route permit all

--- src/main/java/batch/batchapplication/config/SecurityConfig.java
+++ src/main/java/batch/batchapplication/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                                     "/static/**", "/css/**", "/js/**", "/images/**", "/favicon.*").permitAll()  // 정적 리소스 접근 허용
 //                        .requestMatchers("/", "/posts", "/auth/signup", "/auth/login", "/favicon.ico").permitAll()
 //                            .requestMatchers("/posts/[0-9]+").permitAll()  // regex로 숫자로 된 게시글 조회만 허용
+                            .requestMatchers("/actuator/**").permitAll()
                             .requestMatchers("/**").authenticated()
                             .anyRequest().authenticated()
             )
