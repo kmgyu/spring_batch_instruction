4762a63d74e97d6ed1954e838f55475461d1f2ae
kmgyu <lidersy961@gmail.com>
2025-10-27 14:23:13 +0900

    feat: cache manager checker

---
+++ src/main/java/batch/batchapplication/board/CacheCheckController.java
@@ -0,0 +1,20 @@
+package batch.batchapplication.board;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.CacheManager;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/cache")
+@Controller
+@RequiredArgsConstructor
+public class CacheCheckController {
+  CacheManager cacheManager;
+
+  @GetMapping("/check")
+  public ResponseEntity<?> printCacheType() {
+    return ResponseEntity.ok("Current CacheManager: " + cacheManager.getClass().getName());
+  }
+}
