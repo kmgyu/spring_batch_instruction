b5d2399a68aa69d83a64f341910555370cc2e14f
kmgyu <lidersy961@gmail.com>
2025-10-27 13:29:52 +0900

    feat: 테스트를 위한 지연 시간 추가

--- src/main/java/batch/batchapplication/board/BoardService.java
+++ src/main/java/batch/batchapplication/board/BoardService.java
@@ -15,6 +15,12 @@ public class BoardService {
   private final BoardRepository boardRepository;
 
   public BoardResponseDTO findById(Long id) {
+    try {
+      Thread.sleep(20); // 20ms 대기
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    }
+
     Board board = boardRepository.findById(id).orElse(null);
 
     if  (board == null) {
